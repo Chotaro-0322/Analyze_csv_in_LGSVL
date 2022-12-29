e4b3f4a1715cd4816d4ff2fcec70af28736f22f6
Chotaro-0322 <mf21131@shibaura-it.ac.jp>
2022-12-29 15:40:39 +0900

    modify_actor_loss

--- obst_dist_analize.py
+++ obst_dist_analize.py
@@ -7,10 +7,12 @@ from matplotlib.animation import ArtistAnimation
 import glob 
 from tqdm import tqdm
 
-csv_root = "/home/chohome/Master_research/LGSVL/data_analysis/scenario_5"
-gan_csv_list = glob.glob(os.path.join(csv_root + "/full_data/" + "/scenario_5_gan/*.csv"))
-nogan_csv_list = glob.glob(os.path.join(csv_root +  "/full_data/" + "/scenario_5_nogan/*.csv"))
-print("gan_csv_list", gan_csv_list)
+csv_root = "/home/chohome/Master_research/LGSVL/data_analysis/scenario_0"
+gan_csv_list = glob.glob(os.path.join(csv_root + "/full_data/" + "/scenario_0_gan/*.csv"))
+gan_csv_list.sort()
+nogan_csv_list = glob.glob(os.path.join(csv_root +  "/full_data/" + "/scenario_0_nogan/*.csv"))
+nogan_csv_list.sort()
+# print("gan_csv_list", gan_csv_list)
 
 sum_reward = []
 sum_error2dist = []
@@ -19,19 +21,17 @@ sum_goal_dist = []
 success_error2dist = []
 
 dict_pandas = pd.DataFrame({"episode" : [0], 
-                            "actor_loss" : [0], 
-                            "critic_loss" : [0], 
-                            "discriminator_loss" : [0], 
                             "sum_reward" :[0], 
                             "sum_error2dist" : [0], 
                             "sum_collision" : [0], 
                             "sum_goal_dist" : [0]})
+actor_pandas = pd.DataFrame({})
 
-for i, csv_path in enumerate(tqdm(gan_csv_list[:-1])):
+for i, csv_path in enumerate(tqdm(gan_csv_list)):
     if ("failure" in csv_path) or ("success" in csv_path):
         csv_data = pd.read_csv(csv_path, header=None, skiprows=1).to_numpy()
         # objectとの距離の計算
-        print("csv_data[:, 4] : ", csv_data[1:, 4])
+        # print("csv_data[:, 4] : ", csv_data[1:, 4])
         obj_dist = np.min(csv_data[1:, 4])
         sum_error2dist.append([i, obj_dist])
         if "success" in csv_path:
@@ -45,13 +45,25 @@ for i, csv_path in enumerate(tqdm(gan_csv_list[:-1])):
         # collisionフラグの計算
         collision = np.sum(csv_data[:, 16])
         sum_collision.append([i, collision])
+    else:
+        csv_data = pd.read_csv(csv_path, header=None, skiprows=1).to_numpy()
+        actor_loss = csv_data[:, -1]
+        
+        actor_loss_compensate = []
+        for i, loss in enumerate(actor_loss):
+            if i == 0:
+                actor_loss_compensate.append(None)
+            elif loss != 0.0:
+                 actor_loss_compensate.append(loss)
+            elif loss == 0.0:
+                actor_loss_compensate.append(actor_loss_compensate[i-1])
+        actor_pandas["actor_loss"] = actor_loss_compensate
 
     dict_pandas = dict_pandas.append({"episode" : i,
                                     "sum_reward" : reward, 
                                     "min_error2dist" : obj_dist, 
                                     "sum_collision" : collision, 
-                                    "sum_goal_dist" : goal_dist}, ignore_index=True)
-
+                                    "sum_goal_dist" : goal_dist}, ignore_index=True) 
 
 sum_reward = np.array(sum_reward)
 sum_error2dist = np.array(sum_error2dist)
@@ -61,10 +73,17 @@ sum_goal_dist = np.array(sum_goal_dist)
 
 mean_object_distance = np.mean(success_error2dist)
 dict_pandas["mean_object_distance"] = mean_object_distance
-
 dict_pandas.to_csv(csv_root + "/summarize_gan.csv")
+actor_pandas.to_csv(csv_root + "/summarize_gan_actor_loss.csv")
 print("save to : ", csv_root + "/summarize_gan.csv")
 
+# ------------------------------------------------------------------------------------------------------------------------------------
+dict_pandas = pd.DataFrame({"episode" : [0], 
+                            "sum_reward" :[0], 
+                            "sum_error2dist" : [0], 
+                            "sum_collision" : [0], 
+                            "sum_goal_dist" : [0]})
+actor_pandas = pd.DataFrame({})
 sum_reward = []
 sum_error2dist = []
 sum_collision = []
@@ -88,7 +107,19 @@ for i, csv_path in enumerate(tqdm(nogan_csv_list[:-1])):
         # collisionフラグの計算
         collision = np.sum(csv_data[:, 16])
         sum_collision.append([i, collision])
-
+    else:
+        csv_data = pd.read_csv(csv_path, header=None, skiprows=1).to_numpy()
+        actor_loss = csv_data[:, -1]
+        
+        actor_loss_compensate = []
+        for i, loss in enumerate(actor_loss):
+            if i == 0:
+                actor_loss_compensate.append(None)
+            elif loss != 0.0:
+                 actor_loss_compensate.append(loss)
+            elif loss == 0.0:
+                actor_loss_compensate.append(actor_loss_compensate[i-1])
+        actor_pandas["actor_loss"] = actor_loss_compensate
 
     dict_pandas = dict_pandas.append({"episode" : i,
                                     "sum_reward" : reward, 
@@ -106,6 +137,7 @@ mean_object_distance = np.mean(success_error2dist)
 dict_pandas["mean_object_distance"] = mean_object_distance
 
 dict_pandas.to_csv(csv_root + "/summarize_nogan.csv")
+actor_pandas.to_csv(csv_root + "/summarize_nogan_actor_loss.csv")
 print("save to : ", csv_root + "/summarize_nogan.csv")
 
 fig_actor_loss = plt.figure()
